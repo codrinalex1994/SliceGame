6984c25c76459da560ae8156dd1603f3e3c79855
codrinalex1994 <codrinalex1994@yahoo.com>
2018-08-16 23:04:16 +0300

    Shape Health, Destroy, Slice effect

---
+++ SliceGame/Assets/Scripts/ShapeHeart.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeHeart : MonoBehaviour {
+
+    private int health = 50;
+    private Vector2 enteredPosition;
+    private Vector2 exitedPosition;
+    private float sliceLength;
+    private float minSliceLength = 0.4f;
+
+    private Vector3 shapeScale;
+    private Vector3 hitShapeScale;
+    private Vector3 scaleFactor;
+
+    private void Start()
+    {
+        shapeScale = this.transform.localScale;
+        hitShapeScale = shapeScale + (shapeScale * 1 / 8);
+        scaleFactor = shapeScale * 1 / 2;
+    }
+
+    private void Update()
+    {
+        if( this.transform.localScale.magnitude > shapeScale.magnitude)
+        {
+            this.transform.localScale -= scaleFactor * Time.deltaTime; 
+        } 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        enteredPosition = collision.attachedRigidbody.transform.position;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        exitedPosition = collision.attachedRigidbody.transform.position;
+        sliceLength = (exitedPosition - enteredPosition).magnitude;
+        if( sliceLength > minSliceLength)
+        {
+            this.transform.localScale = hitShapeScale;
+            health--;
+        }
+        if( health < 1)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
